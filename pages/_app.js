ba2df8035f11a941076eaed9ff3a2691979bfd70
Khor Zhen Win <78037711+khorzhenwin@users.noreply.github.com>
2023-05-26 23:25:37 +0800

    Initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,50 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Head from "next/head";
+import { ColorSchemeProvider, MantineProvider } from "@mantine/core";
+import { extendTheme, ColorModeScript, ChakraProvider } from "@chakra-ui/react";
+import { useState } from "react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const theme = extendTheme({
+    config: {
+      initialColorMode: "dark",
+      useSystemColorMode: false,
+    },
+  });
+  const [colorScheme, setColorScheme] = useState("dark");
+  const toggleColorScheme = (value) =>
+    setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
+
+  return (
+    <>
+      <Head>
+        <title>AlpacaThings</title>
+        <meta property="og:title" content="AlpacaThings" />
+        <meta
+          property="og:description"
+          content="AlpacaThings | A revolution to waste management"
+        />
+        <meta property="og:image" content="./alpacaverse.png" />
+        <link rel="icon" href="./alpacaverse.png" />
+      </Head>
+
+      <ColorSchemeProvider
+        colorScheme={colorScheme}
+        toggleColorScheme={toggleColorScheme}
+      >
+        <MantineProvider
+          withGlobalStyles
+          withNormalizeCSS
+          theme={{ colorScheme }}
+        >
+          <ChakraProvider theme={theme}>
+            <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+            <Component {...pageProps} />
+          </ChakraProvider>
+        </MantineProvider>
+      </ColorSchemeProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
